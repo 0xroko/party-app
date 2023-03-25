3166db7f2b3728d62fb10a0b042d0f923d248adf
Roko <roko.bekavac2003@gmail.com>
2023-03-25 14:50:56 +0100

    add rel imports

--- apps/mobile/babel.config.js
+++ apps/mobile/babel.config.js
@@ -3,5 +3,20 @@ module.exports = function (api) {
   return {
     presets: ["babel-preset-expo", "nativewind/babel"],
     // plugins: ["nativewind/babel"],
+    plugins: [
+      [
+        require.resolve("babel-plugin-module-resolver"),
+        {
+          root: ["./src"],
+          alias: {
+            "@components": "./src/components",
+            "@screens": "./src/screens",
+            "@utils": "./src/utils",
+            "@assets": "./src/assets",
+            "@lib": "./src/lib",
+          },
+        },
+      ],
+    ],
   };
 };
