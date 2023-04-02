115f633b605107e57b065c8dd0eb8b14439d943b
Roko <roko.bekavac2003@gmail.com>
2023-04-02 18:27:02 +0200

    fix auth stuff

--- apps/mobile/src/features/auth/info-screen.tsx
+++ apps/mobile/src/features/auth/info-screen.tsx
@@ -4,54 +4,75 @@ import { Input } from "@components/input";
 
 import { SafeArea } from "@components/safe-area";
 import { InfoSectionForm, Section, SectionTitle } from "@features/auth/signup";
-import { useLoginStore } from "@lib/actions/auth";
+import { logOut, useLoginStore } from "@lib/actions/auth";
 import { checkIfDisplayNameExists, createUser } from "@lib/actions/user";
+import { useAuthStore } from "@navigation/authStore";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import { Field, Form } from "houseform";
 import { FC } from "react";
 import { z } from "zod";
 
+export const lowerCaseSchema = z.string().superRefine((v, _ctx) => {
+  if (v.toLowerCase() !== v) {
+    _ctx.addIssue({
+      code: "custom",
+      message: "Samo mala slova su dozvoljena",
+    });
+    return z.NEVER;
+  }
+});
+
 export const displayNameSchema = (ignoreId?: string) =>
-  z.string().superRefine(async (v, _ctx) => {
-    if (!v || v.length === 0) {
-      _ctx.addIssue({
-        code: "custom",
-        message: "Obavezno!",
-      });
-      return z.NEVER;
-    }
+  z
+    .string()
+    .toLowerCase()
+    .max(20, "Maksimalno 20 znakova!")
+    .superRefine(async (v, _ctx) => {
+      if (!v || v.length === 0) {
+        _ctx.addIssue({
+          code: "custom",
+          message: "Obavezno!",
+        });
+        return z.NEVER;
+      }
 
-    if (v.includes("@")) {
-      _ctx.addIssue({
-        code: "custom",
-        message: "Korisničko ime ne može početi sa @",
-      });
-      return z.NEVER;
-    }
+      if (v.includes("@")) {
+        _ctx.addIssue({
+          code: "custom",
+          message: "Korisničko ime ne može početi sa @",
+        });
+        return z.NEVER;
+      }
 
-    const exists = await checkIfDisplayNameExists(v, ignoreId);
+      const exists = await checkIfDisplayNameExists(v, ignoreId);
+      if (exists) {
+        _ctx.addIssue({
+          code: "custom",
+          message: "Korisničko ime već postoji, izaberi neko treće",
+        });
+        return z.NEVER;
+      }
 
-    // do this on server
-    // but also if it contains unallowed characters
-    if (exists) {
-      _ctx.addIssue({
-        code: "custom",
-        message: "Korisničko ime već postoji, izaberi neko treće",
-      });
-      return z.NEVER;
-    }
+      // do this on server
+      // but also if it contains unallowed characters
 
-    return v;
-  });
+      return v;
+    });
 
 export const InfoSection = () => {
   const loginState = useLoginStore();
 
+  const setAuthState = useAuthStore((s) => s.setAuthState);
+
   const onSubmit = async (values: InfoSectionForm) => {
     const user = await createUser(values);
+    if (user) {
+      setAuthState("SIGNED_IN");
+    }
   };
 
   const back = () => {
+    logOut(false);
     loginState.setLoginState("PHONE");
   };
 
@@ -65,7 +86,7 @@ export const InfoSection = () => {
       <Form<InfoSectionForm> onSubmit={onSubmit}>
         {({ isValid, submit, isSubmitted }) => (
           <Div className={`flex`}>
-            <Div className={`flex flex-row mb-4 grow`}>
+            <Div className={`flex flex-row grow mb-3`}>
               <Field
                 initialValue={loginState.infoSectionFormData?.displayname}
                 name="displayname"
@@ -166,6 +187,7 @@ export const InfoSection = () => {
                   onBlurValidate={z
                     .string()
                     .nonempty("Obavezno!")
+                    .max(20, "Maksimalno 20 znakova!")
                     .superRefine((v, _ctx) => {
                       return v;
                     })}
@@ -189,7 +211,7 @@ export const InfoSection = () => {
               </Div>
             </Div>
 
-            <Div className={`flex gap-3 flex-row mt-8`}>
+            <Div className={`flex g-3 flex-row mt-12`}>
               <Div className={`flex basis-[33%] grow-0`}>
                 <Button onPress={back} size={"medium"} intent={"secondary"}>
                   Natrag
